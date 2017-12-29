a1f621602c5c8f7d8d84fd9ed1ee3e5a96d24d64
code2501 <jisuyonsei@gmail.com>
2017-12-29 20:25:55 +0900

    save

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,13 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 import os
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
-]
+
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
